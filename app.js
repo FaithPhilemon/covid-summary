52933e5bfc323b570456e2974763780b39dbb47b
Faith Philemon <philemonfaith@gmail.com>
2020-04-08 01:07:35 -0700

    set GET request and response working

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
+const https = require('https');
+
 // Problem: We need a simple way to view COVID-19 Data without clicks and chnage of tabs.
 // Solution: Use NodeJs to connect to postman's api to get covid 19 information.
 
+function print_info(total_cases, recovered, deaths){
+    let message = `Total Cases: ${total_cases}, Recovered ${recovered}, Total deaths ${deaths}`;
+
+    console.log(message);
+}
+
+
+const req = https.get('https://api.covid19api.com/summary', res => {
+
+    let body = "";
+
+     //  Read data
+    res.on('data', (data) =>{
+        body += data.toString();
+    });
+
+    res.on('end', ()=>{
+         //  Parse the data and 
+        const covid19 = JSON.parse(body);
+        console.dir(covid19);
+    });
+   
+   
+    // Print the data
+
+});
